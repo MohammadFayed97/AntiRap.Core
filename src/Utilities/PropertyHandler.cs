26396054e40b49b707245b15448cc3e4d2a3fa69
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-06-01 16:36:02 +0200

    change PropertyHandler to static

--- src/Utilities/PropertyHandler.cs
+++ src/Utilities/PropertyHandler.cs
@@ -1,26 +1,25 @@
 ﻿namespace AntiRap.Core.Utilities
 {
-    using AntiRap.Core.Abstractions;
     using System;
     using System.Reflection;
 
-    public class PropertyHandler : IPropertyHandler
+    public static class PropertyHandler
     {
-        public object GetPropertyValue(object modelObject, string propertName) => GetProperty(modelObject, propertName)?.GetValue(modelObject, null);
+        public static object GetPropertyValue(object modelObject, string propertName) => GetProperty(modelObject, propertName)?.GetValue(modelObject, null);
 
-        public Type GetPropertyType(object model, string propertyName) => GetProperty(model, propertyName)?.PropertyType;
-        public Type GetPropertyType<TModel>(string propertyName) => GetProperty<TModel>(propertyName).PropertyType;
+        public static Type GetPropertyType(object model, string propertyName) => GetProperty(model, propertyName)?.PropertyType;
+        public static Type GetPropertyType<TModel>(string propertyName) => GetProperty<TModel>(propertyName).PropertyType;
 
-        public void SetPropertyValue(object model, string propertyName, object value) => GetProperty(model, propertyName)?.SetValue(model, value);
+        public static void SetPropertyValue(object model, string propertyName, object value) => GetProperty(model, propertyName)?.SetValue(model, value);
 
-        public PropertyInfo GetProperty(object model, string propertyName)
+        public static PropertyInfo GetProperty(object model, string propertyName)
         {
             if (string.IsNullOrWhiteSpace(propertyName))
                 return null;
 
             return model?.GetType().GetProperty(propertyName);
         }
-        public PropertyInfo GetProperty<TModel>(string propertyName)
+        public static PropertyInfo GetProperty<TModel>(string propertyName)
         {
             if (string.IsNullOrWhiteSpace(propertyName))
                 return null;
